556738b79b30d0bb3a2f23729afafe6d0fe03d22
Clementine Oldfield <clementine.oldfield@gmail.com>
2019-04-17 15:57:07 +1000

    added form structure

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,13 +6,15 @@ class ListingsController < ApplicationController
   end
 
   def create; end
-  def new; end
+  def new
+    @listing = Listing.new
+  end
 
   def show; end
 
   def edit; end
   def update; end
-  
+
   def destroy; end
 
   private
