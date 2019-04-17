6022f060db86c635de92d4a528a45d0444a982d2
Clementine Oldfield <clementine.oldfield@gmail.com>
2019-04-17 15:25:42 +1000

    Added before_action and methods to controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,24 @@
 class ListingsController < ApplicationController
-  def index; end
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @listings = Listing.all
+  end
+
   def create; end
   def new; end
-  def edit; end
+
   def show; end
+
+  def edit; end
   def update; end
+  
   def destroy; end
+
+  private
+
+  def set_listing
+    @id = params[:id]
+    @listing = Listing.find(id)
+  end
 end
