a3cae070a438daaa53d27cc0af6b74bec2ad3b37
Clementine Oldfield <clementine.oldfield@gmail.com>
2019-04-23 15:14:51 +1000

    Added in authorization

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,7 @@
 class ListingsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :update, :destroy]
   before_action :set_breeds_and_sexes, only: [:new, :edit]
   
   def index
@@ -8,8 +9,8 @@ class ListingsController < ApplicationController
   end
 
   def create
-    @listing = Listing.create(listing_params)
-    @listing.errors.any? 
+    @listing = current_user.listings.create(listing_params)
+
     if @listing.errors.any?
       set_breeds_and_sexes
       render "new"
@@ -45,4 +46,11 @@ class ListingsController < ApplicationController
     @id = params[:id]
     @listing = Listing.find(@id)
   end
+
+  def authorize_user
+    if @listing.user_id != current_user.id
+      redirect_to listings_path
+    end
+  end
+
 end
